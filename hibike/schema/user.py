0f58dc4925bc2984eef385496f07dd4c2ee7c6fd
eui9179 <outdark97@gmail.com>
2022-04-23 02:31:29 +0900

    fix/422

--- hibike/schema/user.py
+++ hibike/schema/user.py
@@ -79,7 +79,7 @@ class RequestReplySchema(Schema):
       post_id = fields.Int(description="포스트 id", requird=True)      
 
 class RequestRidingAveSchema(Schema):
-      user_id = fields.Int(description="유저 아이디", required=True)
+      user_id = fields.Str(description="유저 아이디", required=True)
       riding_time = fields.Str(description="주행 시간", required=True)
       ave_speed = fields.Str(description="평균 속도", required=True)
       ave_distence = fields.Str(description="평균 거리", required=True)
