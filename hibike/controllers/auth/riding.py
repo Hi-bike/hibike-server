832f0e3b9c39d51e3caf5c1c0c8116e122df0620
eui9179 <outdark97@gmail.com>
2022-05-01 02:51:12 +0900

    fix/time-desc

--- hibike/controllers/auth/riding.py
+++ hibike/controllers/auth/riding.py
@@ -57,7 +57,7 @@ def get_riding_info_one(unique_id):
 )
 def create_riding(user_id, unique_id, riding_time, ave_speed, distance): #, starting_point, end_point):
     KST = timezone('Asia/Seoul')
-    time = datetime.now().astimezone(KST).strftime('%Y-%m-%d')
+    time = datetime.now().astimezone(KST).strftime('%Y-%m-%d %H:%M:%S')
     
     if riding_time == "nan" and distance == "nan":
         return response_json_with_code()
