84d76e261bcdedfed18d620d29a75a4e7894a9db
eui9179 <outdark97@gmail.com>
2022-05-12 04:10:09 +0900

    test-end

--- hibike/controllers/board/danger.py
+++ hibike/controllers/board/danger.py
@@ -215,9 +215,9 @@ def del_my_danger(user_id, latitude, longitude, my_latitude, my_longitude):
             danger_row = db.session.query(Danger).filter((Danger.latitude == latitude) & (Danger.longitude == longitude)).first()
             danger_row.is_delete = 'Y'
             db.session.commit()
-            return response_json_with_code(200, result=str(haversine(mark_location, my_location, unit = 'm')))
+            return response_json_with_code(200, result="success")
         else:
-            return response_json_with_code(200, result=str(haversine(mark_location, my_location, unit = 'm')))
+            return response_json_with_code(200, result="fail")
                 
     
 @board_bp.route("/dimage/<filename>", methods=["GET"])
